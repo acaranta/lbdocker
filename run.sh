b333f07c1b1cd67d75c52212501f42f3ddfcb0d2
acaranta <arthur@caranta.com>
2020-02-14 02:07:43 +0100

    added automatic log symlink to current container

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+cd $LOGSPATH 
+rm -f current 2>&1 >/dev/null
+ln -s $DOCKERID current
 
+cd /app
 cp /hacfg/$HASVC /etc/haproxy/$HASVC
 if [ $? -gt 0 ]; then
 	echo "Configuration file /hacfg/hapconf.cfg was not found !!!"
